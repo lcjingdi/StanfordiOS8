3cd3ec0a1066a5f4572f29ed241adb3b23b381a2
lcjingdi <lcjingdi@gmail.com>
2016-10-14 09:05:14 +0800

    test2

--- SF_Calculator/SF_Calculator/ViewController.swift
+++ SF_Calculator/SF_Calculator/ViewController.swift
@@ -26,6 +26,44 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func operate(_ sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        switch operation {
+        case "+": performOperation {$0 + $1}
+        case "−": performOperation {$1 - $0}
+        case "×": performOperation {$0 * $1}
+        case "÷": performOperation {$1 / $0}
+        default:
+            break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+
+    @IBAction func enter() {
+        operandStack.append(displayValue)
+        userIsInTheMiddleOfTypingANumber = false
+        print("operandStack \(operandStack)")
+    }
+    var operandStack = [Double]()
+    
+    var displayValue: Double {
+        get {
+            return NumberFormatter().number(from: display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+        }
+    }
 
 }
 
